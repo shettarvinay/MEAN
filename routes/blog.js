76870023f40054a8a46c322a0722f14cef3fe107
shettarvinay <shettarvinay@gmail.com>
2016-06-09 14:13:42 +0530

    show, update done

--- routes/blog.js
+++ routes/blog.js
@@ -37,5 +37,35 @@ router.get('/list_all_blogs',function(req,res){
 	})
 })
 
+router.put('/edit/:_id',function(req,res){
+	console.log("This is id sent")
+	console.log(req.params._id);
+	var _id=req.params._id;
+	Blog.find({_id : _id},function(err,doc){
+		res.render('edit_blog',{
+			blog : doc
+		})
+	})
+})
+
+router.post('/update_blog',function(req,res){
+	var _id = req.body._id;
+	var desc = req.body.desc;
+
+	console.log(req.body._id)
+
+	Blog.update({_id : _id},{ description : desc},function(err,raw){
+		if(raw.ok){
+			Blog.find({_id : _id},function(err,doc){
+				console.log('The raw response from Mongo was ', doc);
+				res.render('show_blog',{
+					blog : doc
+				});
+			});	
+		}
+		
+	});
+});
+
 module.exports = router;
 
